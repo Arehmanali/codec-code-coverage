4bc659bea62d0777ebff0f0ff03557434f205723
Rehman Ali <arehmanali.uet@gmail.com>
2024-02-24 00:00:45 +0500

    update

--- test/hello_world.test.ts
+++ test/hello_world.test.ts
@@ -1,7 +1,7 @@
 import { hello } from "../src/hello_world";
 
 describe("test hello", () => {
-  it("should return hello world", () => {
+  it("should return hello world in test", () => {
     expect(hello()).toBe("Hello world!");
   });
 });
